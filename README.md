3a41dbabf7f2df79124cd5b30aa7f099be2586ae
Ankit Gohel <ankitgohel1996@gmail.com>
2016-07-03 02:43:12 +0530

    Fixed site for mobile

--- README.md
+++
@@ -1,2 +0,0 @@
-# Sadgi-Website
-One page website for Sadgi Creative <br> Can be viewed at https://sadgi.in/
--- circle.js
+++ circle.js
@@ -1,4 +1,182 @@
+if($(window).width() >= 450)
+{
+var el = $('#circle0'), inited = false;
 
+el.appear({ force_process: true });
+
+el.on('appear', function() 
+{
+  if (!inited) 
+  {
+    el.circleProgress(
+    { 
+      size:120,
+      thickness:11,
+      fill:{color:"#F7EC37" }
+    }).on('circle-animation-progress', function (event,progress,value) 
+    {
+      $('#value0')  .text((value * 100).toFixed()+"%");
+    });
+    inited = true;
+  }
+});
+
+var el1 = $('#circle1'), inited1 = false;
+
+el1.appear({ force_process: true });
+
+el1.on('appear', function() 
+{
+  if (!inited1) 
+  {
+    el1.circleProgress(
+    {
+      size:120,
+      thickness:11,
+      fill:{color:"#F7EC37"}
+    }).on('circle-animation-progress', function (event, progress,value) 
+    {
+      $('#value1')  .text((value * 100).toFixed()+"%");
+    });
+    inited1 = true;
+  }
+});
+
+var el2 = $('#circle2'), inited2 = false;
+
+el2.appear({ force_process: true });
+
+el2.on('appear', function()
+{
+  if (!inited2) 
+  {
+    el2.circleProgress(
+    {
+      size:120,
+      thickness:11,
+      fill:{color:"#F7EC37"}
+    }).on('circle-animation-progress', function (event, progress,value) 
+    {
+      $('#value2')  .text((value * 100).toFixed()+"%");
+    });
+    inited2 = true;
+  }
+});
+
+var el3 = $('#circle3'), inited3 = false;
+
+el3.appear({ force_process: true });
+
+el3.on('appear', function() 
+{
+  if (!inited3) 
+  {
+    el3.circleProgress(
+    {
+      size:120,
+      thickness:11,
+      fill:{color:"#F7EC37"}
+    }).on('circle-animation-progress', function (event, progress,value) 
+    {
+      $('#value3').text((value * 100).toFixed()+"%");
+    });
+    inited3 = true;
+  }
+}); 
+}
+
+else 
+{
+  var el = $('#circle0'), inited = false;
+
+el.appear({ force_process: true });
+
+el.on('appear', function() 
+{
+  if (!inited) 
+  {
+    el.circleProgress(
+    { 
+      size:90,
+      thickness:8,
+      fill:{color:"#F7EC37" }
+    }).on('circle-animation-progress', function (event,progress,value) 
+    {
+      $('#value0')  .text((value * 100).toFixed()+"%");
+    });
+    inited = true;
+  }
+});
+
+var el1 = $('#circle1'), inited1 = false;
+
+el1.appear({ force_process: true });
+
+el1.on('appear', function() 
+{
+  if (!inited1) 
+  {
+    el1.circleProgress(
+    {
+      size:90,
+      thickness:8,
+      fill:{color:"#F7EC37"}
+    }).on('circle-animation-progress', function (event, progress,value) 
+    {
+      $('#value1')  .text((value * 100).toFixed()+"%");
+    });
+    inited1 = true;
+  }
+});
+
+var el2 = $('#circle2'), inited2 = false;
+
+el2.appear({ force_process: true });
+
+el2.on('appear', function()
+{
+  if (!inited2) 
+  {
+    el2.circleProgress(
+    {
+      size:90,
+      thickness:8,
+      fill:{color:"#F7EC37"}
+    }).on('circle-animation-progress', function (event, progress,value) 
+    {
+      $('#value2')  .text((value * 100).toFixed()+"%");
+    });
+    inited2 = true;
+  }
+});
+
+var el3 = $('#circle3'), inited3 = false;
+
+el3.appear({ force_process: true });
+
+el3.on('appear', function() 
+{
+  if (!inited3) 
+  {
+    el3.circleProgress(
+    {
+      size:90,
+      thickness:8,
+      fill:{color:"#F7EC37"}
+    }).on('circle-animation-progress', function (event, progress,value) 
+    {
+      $('#value3').text((value * 100).toFixed()+"%");
+    });
+    inited3 = true;
+  }
+}); 
+}
+
+
+$(window).resize(function() {
+
+if($(window).width() >= 450)
+{
 var el = $('#circle0'), inited = false;
 
 el.appear({ force_process: true });
@@ -82,3 +260,92 @@ el3.on('appear', function()
     inited3 = true;
   }
 });	
+}
+
+else 
+{
+  var el = $('#circle0'), inited = false;
+
+el.appear({ force_process: true });
+
+el.on('appear', function() 
+{
+  if (!inited) 
+  {
+    el.circleProgress(
+    { 
+      size:90,
+      thickness:8,
+      fill:{color:"#F7EC37" }
+    }).on('circle-animation-progress', function (event,progress,value) 
+    {
+      $('#value0')  .text((value * 100).toFixed()+"%");
+    });
+    inited = true;
+  }
+});
+
+var el1 = $('#circle1'), inited1 = false;
+
+el1.appear({ force_process: true });
+
+el1.on('appear', function() 
+{
+  if (!inited1) 
+  {
+    el1.circleProgress(
+    {
+      size:90,
+      thickness:8,
+      fill:{color:"#F7EC37"}
+    }).on('circle-animation-progress', function (event, progress,value) 
+    {
+      $('#value1')  .text((value * 100).toFixed()+"%");
+    });
+    inited1 = true;
+  }
+});
+
+var el2 = $('#circle2'), inited2 = false;
+
+el2.appear({ force_process: true });
+
+el2.on('appear', function()
+{
+  if (!inited2) 
+  {
+    el2.circleProgress(
+    {
+      size:90,
+      thickness:8,
+      fill:{color:"#F7EC37"}
+    }).on('circle-animation-progress', function (event, progress,value) 
+    {
+      $('#value2')  .text((value * 100).toFixed()+"%");
+    });
+    inited2 = true;
+  }
+});
+
+var el3 = $('#circle3'), inited3 = false;
+
+el3.appear({ force_process: true });
+
+el3.on('appear', function() 
+{
+  if (!inited3) 
+  {
+    el3.circleProgress(
+    {
+      size:90,
+      thickness:8,
+      fill:{color:"#F7EC37"}
+    }).on('circle-animation-progress', function (event, progress,value) 
+    {
+      $('#value3').text((value * 100).toFixed()+"%");
+    });
+    inited3 = true;
+  }
+}); 
+}
+}); 
